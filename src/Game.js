d49a0131d48c0e5517200edb01ce7b86e4fb2a61
Mahmoud Hammad <hodaman2012@gmail.com>
2022-05-21 07:51:54 +0200

    L works, z c t not

--- src/Game.js
+++ src/Game.js
@@ -47,27 +47,83 @@ const gameArray = [
     
 
 useEffect(() => {
-    FindRowIndex(1,2)
+    // const test = [[1,2],[1,0],[1,3]] 
+    // // [1,2]
+
+    //         let counter=0
+    //         test.forEach(s=>{
+    //             if(s[0] == 1){
+    //                 console.log("YSS",counter)
+    //                 console.log("s",s)
+                    
+    //                 counter++
+    //             }
+    //         })
+    // FindRowIndex(1,2)
   }, []);
 
   const DrawBorder = (rowIndex, colIndex)=>{
-    let top = 0 , left=0
+    let top = 1 , left=1
     const groupIndex = FindRowIndex(rowIndex,colIndex)
     const groubbbb = gameArray[groupIndex]
     const sortedx = groubbbb.sort((a,b)=>a[0]-b[0])
     const sorted = sortedx.sort((a,b)=>a[1]-b[1])
-    console.log("FFFFF",sorted )
-    const [minY, minX] = sorted[0]
+    const [minRow, minCol] = sorted[0]
+    const [maxRow, maxCol] = sorted[sorted.length-1]
     //  sorted.forEach(s=>{
 
     //  })
-    if(colIndex==minX){
+
+    // (1,3)
+    const test = [[1,2],[1,0],[1,3]] 
+    // [1,2]
+
+            let counter=0
+            sorted.forEach(s=>{
+                if(s[0] == rowIndex){ 
+                    counter++
+                }
+            })
+            let fuck=0
+            sorted.forEach(s=>{
+                if(s[1] == colIndex){ 
+                    fuck++
+                }
+            })
+         
+    // console.log("Lonn",lonely) 
+    // const onlyrows = sorted.map((s,index)=>{
+    //     const f =  sorted.filter(filteredS=>{
+    //         if(filteredS[1] ==sorted[index][1]){
+    //             console.log(filteredS,sorted[index])
+    //         }
+    //         return filteredS[1] ==sorted[index][1]
+    //     })
+    //     return []
+    //     // console.log("FFa7aF",f)
+
+    // })
+   const ff  =  [
+        [
+           [0,2],
+           [0,3]
+        ],[
+            [1,3]
+        ]
+    ]
+    if( colIndex>minCol){
+        left=0
+    }
+    if( counter==1){
         left=1
     }
-    if(rowIndex==minY){
+    if(rowIndex>minRow){
+        top=0
+    }
+    if(fuck ==1){
         top=1
     }
-    return [top ,left]
+    return [top,left]
   }
   return (
 <Box sx={{
@@ -95,9 +151,9 @@ useEffect(() => {
                             ${(rann1/1+rann1*FindRowIndex(rowIndex,colIndex))%190},
                             ${rann2/10+((rann2 *FindRowIndex(rowIndex,colIndex)))%100},
                             ${(rann3/1+rann3 *FindRowIndex(rowIndex,colIndex))%205})`,
-
-                            borderTop:`${BT*8}px solid #fff`, 
-                            borderLeft:`${BL*8}px solid #fff`,
+                            borderTop:"8px solid #fff",
+                            borderTop:`${ BT*8}px solid #fff`, 
+                            borderLeft:`${ BL*8}px solid #fff`,
                             
                             padding:"0px 20px 20px",  flexDirection: "column", justifyContent:"space-around"}}>
                             {/* {FindRowIndex(rowIndex,colIndex)} */}
