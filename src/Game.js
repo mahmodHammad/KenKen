8629ea09d09567fdf95102931cdfe3adcf00ddf2
Mahmoud Hammad <hodaman2012@gmail.com>
2022-05-21 16:48:11 +0200

    drawing cage is DONEEE!

--- src/Game.js
+++ src/Game.js
@@ -33,98 +33,70 @@ export default function App() {
 // [[1, 2], [2, 2], [2, 3], '12*'],
 // [[3, 3], '2']] 
 const gameArray = [
-    [[0, 0], [0, 1]],
+    [[0, 0], [0, 1],[1, 1],[1, 2]],
     [[0, 2], [0, 3], [1, 3]],
     [[1, 0], [2, 0], [3, 0]],
-    [[1, 1], [2, 1], [3, 1], [3, 2]],
-    [[1, 2], [2, 2], [2, 3]],
+    [  [3, 1], [3, 2]],
+    [ [2, 1],[2, 2], [2, 3]],
     [[3, 3]]
 ] 
-/* 
 
-*/
+// Do we have top or left neignbour (if yes don't drow)
+ 
+
     const FindRowIndex = (x,y)=>gameArray.findIndex(f=>f.findIndex(g=>g[0]===x && g[1]===y)!==-1)
     
 
 useEffect(() => {
-    // const test = [[1,2],[1,0],[1,3]] 
-    // // [1,2]
-
-    //         let counter=0
-    //         test.forEach(s=>{
-    //             if(s[0] == 1){
-    //                 console.log("YSS",counter)
-    //                 console.log("s",s)
-                    
-    //                 counter++
-    //             }
-    //         })
-    // FindRowIndex(1,2)
+    // const t = [ [2, 1],[2, 2], [2, 3]]
+    // hasNeigbour([2,2],t)
+    
   }, []);
+
+  const hasNeigbour = (item,group)=>{
+    //   const gtest = [[0, 0], [1, 0],[0, 1],[1, 1],[1, 2]]
+    const [row,col] = item
+    const sameRow = group.filter(g=>(g[0]==row ))
+    const sameCol = group.filter(g=>(g[1]==col ))
+
+    console.log("S",sameRow)
+    console.log("col",col)
+    let hasLeft , hasTop
+    if(sameRow.length>1){
+          hasLeft =  sameRow.findIndex(s=>{
+            // console.log( col,s[1])
+            // console.log(col==s[1]-1 , col==s[1]+1)
+           
+            return col==s[1]+1
+          }) !==-1 ? 0 : 1
+    }else{
+        console.log("items",item )
+        hasLeft = 1
+    }
+    if(sameCol.length>1){
+        hasTop = sameCol.findIndex(s=>row==s[0]+1)!==-1 ? 0 : 1
+    }else{
+        hasTop = 1
+    } 
+    console.log(hasTop,hasLeft)
+   return [hasLeft,hasTop]
+  }
 
   const DrawBorder = (rowIndex, colIndex)=>{
     let top = 1 , left=1
     const groupIndex = FindRowIndex(rowIndex,colIndex)
     const groubbbb = gameArray[groupIndex]
-    const sortedx = groubbbb.sort((a,b)=>a[0]-b[0])
-    const sorted = sortedx.sort((a,b)=>a[1]-b[1])
-    const [minRow, minCol] = sorted[0]
-    const [maxRow, maxCol] = sorted[sorted.length-1]
-    //  sorted.forEach(s=>{
-
-    //  })
-
-    // (1,3)
-    const test = [[1,2],[1,0],[1,3]] 
-    // [1,2]
-
-            let counter=0
-            sorted.forEach(s=>{
-                if(s[0] == rowIndex){ 
-                    counter++
-                }
-            })
-            let fuck=0
-            sorted.forEach(s=>{
-                if(s[1] == colIndex){ 
-                    fuck++
-                }
-            })
-         
-    // console.log("Lonn",lonely) 
-    // const onlyrows = sorted.map((s,index)=>{
-    //     const f =  sorted.filter(filteredS=>{
-    //         if(filteredS[1] ==sorted[index][1]){
-    //             console.log(filteredS,sorted[index])
-    //         }
-    //         return filteredS[1] ==sorted[index][1]
-    //     })
-    //     return []
-    //     // console.log("FFa7aF",f)
-
-    // })
-   const ff  =  [
-        [
-           [0,2],
-           [0,3]
-        ],[
-            [1,3]
-        ]
-    ]
-    if( colIndex>minCol){
-        left=0
-    }
-    if( counter==1){
-        left=1
-    }
-    if(rowIndex>minRow){
-        top=0
-    }
-    if(fuck ==1){
-        top=1
-    }
+    // const sortedx = groubbbb.sort((a,b)=>a[0]-b[0])
+    // const sorted = sortedx.sort((a,b)=>a[1]-b[1])
+    // const [minRow, minCol] = sorted[0] 
+    
+    const result = hasNeigbour([rowIndex,colIndex],groubbbb)
+    top = result[1]
+    left= result[0]
+    
     return [top,left]
   }
+
   return (
 <Box sx={{
     display:"flex",
@@ -142,10 +114,10 @@ useEffect(() => {
 
 
 }}>
-        {gameData.map((row,rowIndex)=><Grid container sx={{}}>
+        {gameData.map((row,rowIndex)=><Grid key={rowIndex} container >
                     {row.map((col,colIndex)=>{
                         const [BT ,BL] = DrawBorder(rowIndex, colIndex)
-                        return(<Grid item sx={{display:"inline-flex",
+                        return(<Grid key={colIndex} item sx={{display:"inline-flex",
                             width:"150px",height:"150px", border:"1px solid #999", 
                             background:`rgb(
                             ${(rann1/1+rann1*FindRowIndex(rowIndex,colIndex))%190},
