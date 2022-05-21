527117214b3211a1c7d84e275bb0999b008edc8a
Mahmoud Hammad <hodaman2012@gmail.com>
2022-05-21 04:50:02 +0200

    عبث

--- src/Game.js
+++ src/Game.js
@@ -3,14 +3,28 @@ import React, {useEffect,useState } from "react";
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
  
- 
+ const rann1 =Math.round(Math.random(0)*100)
+ const rann2 =Math.round(Math.random(0)*200)
+ const rann3 =Math.round(Math.random(0)*400) 
+
 export default function App() { 
     const gameData = [
         [4, 1, 2, 3],
         [1, 2, 3, 4],
         [2, 3, 4, 1],
-        [3, 4, 1, 2]
+        [3, 4, 1, 2],
     ]
+    // const gameData = [
+    //     [4, 1, 2, 3,4, 1, 2, 3,4, 1],
+    //     [4, 1, 2, 3,4, 1, 2, 3,4, 1],
+    //     [4, 1, 2, 3,4, 1, 2, 3,4, 1],
+    //     [4, 1, 2, 3,4, 1, 2, 3,4, 1],
+    //     [4, 1, 2, 3,4, 1, 2, 3,4, 1],
+    //     [4, 1, 2, 3,4, 1, 2, 3,4, 1],
+    //     [4, 1, 2, 3,4, 1, 2, 3,4, 1],
+    //     [4, 1, 2, 3,4, 1, 2, 3,4, 1],
+    //     [4, 1, 2, 3,4, 1, 2, 3,4, 1], 
+    // ]
 // const gameArray = [
 // [[0, 0], [0, 1], '4.0/'],
 // [[0, 2], [0, 3], [1, 3], '9+'],
@@ -29,11 +43,9 @@ const gameArray = [
 /* 
 
 */
-    const FindRowIndex = (x,y)=>{
-       const gameInn = gameArray.findIndex(gg=>gg.findIndex(g=>g[0] == x && g[1]==y)!==-1)
-        console.log("gameIN",gameInn)
-    }
+    const FindRowIndex = (x,y)=>gameArray.findIndex(f=>f.findIndex(g=>g[0]===x && g[1]===y)!==-1)
     
+
 useEffect(() => {
     FindRowIndex(1,2)
   }, []);
@@ -52,8 +64,8 @@ useEffect(() => {
         borderRadius:"4px"
 }}>
         {gameData.map((row,rowIndex)=><Grid container sx={{}}>
-            {row.map((col,colIndex)=><Grid item xs={3} sx={{display:"inline-flex",
-            width:"150px",height:"150px", border:"1px solid #999", 
+            {row.map((col,colIndex)=><Grid item sx={{display:"inline-flex",
+            width:"150px",height:"150px", border:"1px solid #999",  background:`rgb(${(rann1/3+rann1*FindRowIndex(rowIndex,colIndex))%255},${rann2/2+((rann2 *FindRowIndex(rowIndex,colIndex)))%255},${(rann3/2+rann3 *FindRowIndex(rowIndex,colIndex))%255})`,
             padding:"0px 20px 20px",  flexDirection: "column", justifyContent:"space-around"}}>
                     {/* {FindRowIndex(rowIndex,colIndex)} */}
                     <Box sx={{fontWeight:"bold",fontSize:"26px",color:"#eee"}}>
